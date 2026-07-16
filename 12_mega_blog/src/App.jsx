ed2b24a52e5a50458627058fd9fe41f29b254505
ahamadarbab <ahmadarbab.456@gmail.com>
2026-07-16 18:41:10 +0530

    Build authentication service with appwrite

---
+++ 12_mega_blog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import config from './config/config';
+
+function App() {
+
+  console.log(config.appwriteUrl);
+
+  return (
+    <>
+      <h1>Blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
